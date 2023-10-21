e346044a19b83360f7cf81edde58650b2d1b2e40
mezaaziel06 <mezzaaazielle@gmail.com>
2023-10-21 16:17:41 -0600

    areglo

--- Ut-work/src/App.jsx
+++ Ut-work/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./Componentes/Home"
 import InicioS from "./Componentes/InicioS";
 import NewNote from "./Componentes/NoteNew";
+import NotasP from "./Componentes/NotasP";
 const App = () => {
   return (
     <div>
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/InicioS" element={<InicioS/>}/>
         <Route path="/Nnote" element={<NewNote/>}/>
         <Route path="/Notas" element={<NewNote/>}/>
+        <Route path="/NotasP" element={<NotasP/>}/>
       </Routes>
     </div>
   );
